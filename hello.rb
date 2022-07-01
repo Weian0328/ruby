80fcde21decc3c0858e4d436726a0df4a5643095
weian <kevin19960328@gmail.com>
2022-07-01 18:15:16 +0800

    practice array hash symbol in Ruby

---
+++ hello.rb
@@ -0,0 +1,22 @@
+# lists = [1, 2, 3, 4, 5]
+
+# p lists 印出陣列
+
+# lists.each do |list| 使用迴圈將陣列值印出來
+#   p  list
+# end
+
+# p lists.length 陣列的長度
+
+# lists << 6 ＋在陣列最後面
+# lists.push 6
+# p lists
+# p lists.length
+
+# p lists.last
+
+# p lists.map {|x| x * 2} 對陣列做事組成新的陣列
+
+# p (1..10).select{|x| x < 5} 挑出陣列中符合條件的組成新陣列
+# p (1..10).reject{|x| x < 5} 挑出陣列中不符合條件的組成新陣列
+
