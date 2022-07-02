d3bf19e0bd6bf46cf99dfae43774e6ba1ee9d598
weian <kevin19960328@gmail.com>
2022-07-02 15:44:55 +0800

    method practice

---
+++ method.rb
@@ -0,0 +1,6 @@
+def say_hello_to(someone)
+  puts "hi #{someone}"
+end
+
+say_hello_to("kk")
+say_hello_to "aa"
---
+++ practice01.rb
@@ -0,0 +1,30 @@
+# 1. 請使用迴圈的方式，在畫面上印出以下結果：
+
+# *
+# **
+# ***
+# ****
+# *****
+
+# 1.upto(5) do |n|
+#   puts "*" * n
+# end
+
+# 2. 請完成以下計算潤年的方法：
+
+def is_leap_year?(y)
+  y % 4 == 0 && y % 100 != 0 || y % 400 == 0 
+end
+
+puts is_leap_year?(1998)  # 印出 false
+puts is_leap_year?(2000)  # 印出 true
+puts is_leap_year?(2100)  # 印出 false
+
+# 3. 請讓這個 BMI 計算方法可以顯示期待的結果
+
+# def bmi_calculator(height, weight)
+#   # 實作內容在這裡
+# end
+
+# puts bmi_calculator(170, 50) # 印出 17.3 (小數點以下一位，四捨五入)
+# puts bmi_calculator(180, 65) # 印出 20.1 (小數點以下一位，四捨五入)
