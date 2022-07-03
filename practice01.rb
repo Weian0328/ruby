3321139de142c6c0052a4d9049837ad7005d5a5f
weian <kevin19960328@gmail.com>
2022-07-03 23:40:39 +0800

    complete practice

--- practice01.rb
+++ practice01.rb
@@ -12,19 +12,21 @@
 
 # 2. 請完成以下計算潤年的方法：
 
-def is_leap_year?(y)
-  y % 4 == 0 && y % 100 != 0 || y % 400 == 0 
-end
+# def is_leap_year?(y)
+#   y % 4 == 0 && y % 100 != 0 || y % 400 == 0 
+# end
 
-puts is_leap_year?(1998)  # 印出 false
-puts is_leap_year?(2000)  # 印出 true
-puts is_leap_year?(2100)  # 印出 false
+# puts is_leap_year?(1998)  # 印出 false
+# puts is_leap_year?(2000)  # 印出 true
+# puts is_leap_year?(2100)  # 印出 false
 
 # 3. 請讓這個 BMI 計算方法可以顯示期待的結果
 
-# def bmi_calculator(height, weight)
-#   # 實作內容在這裡
-# end
+def bmi_calculator(height, weight)
+  height = height / 100.0
+  weight = weight 
+  (weight / (height * height)).round(1)
+end
 
-# puts bmi_calculator(170, 50) # 印出 17.3 (小數點以下一位，四捨五入)
-# puts bmi_calculator(180, 65) # 印出 20.1 (小數點以下一位，四捨五入)
+puts bmi_calculator(170, 50) # 印出 17.3 (小數點以下一位，四捨五入)
+puts bmi_calculator(180, 65) # 印出 20.1 (小數點以下一位，四捨五入)
